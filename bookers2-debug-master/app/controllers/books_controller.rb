3a260885a48e59068a943bfe08b7c16eb15bb8b6
einbone921 <einbone921@gmail.com>
2020-12-02 03:40:52 +0000

    [Fix] エラーの修正

--- bookers2-debug-master/app/controllers/books_controller.rb
+++ bookers2-debug-master/app/controllers/books_controller.rb
@@ -3,17 +3,20 @@ class BooksController < ApplicationController
 
   def show
     @book = Book.find(params[:id])
+    @book_new = Book.new
   end
 
   def index
     @books = Book.all
+    @book = Book.new
+    @user =current_user
   end
 
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     if @book.save
-      redirect_to book_path(@book), notice: "You have created book successfully."
+      redirect_to book_path(@book.id), notice: "You have created book successfully."
     else
       @books = Book.all
       render 'index'
@@ -24,8 +27,6 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
   end
 
-
-
   def update
     @book = Book.find(params[:id])
     if @book.update(book_params)
@@ -35,16 +36,16 @@ class BooksController < ApplicationController
     end
   end
 
-  def delete
+  def destroy
     @book = Book.find(params[:id])
-    @book.destoy
+    @book.destroy
     redirect_to books_path
   end
 
   private
 
   def book_params
-    params.require(:book).permit(:title)
+    params.require(:book).permit(:title, :body)
   end
 
 end
--- bookers2-debug-master/app/controllers/users_controller.rb
+++ bookers2-debug-master/app/controllers/users_controller.rb
@@ -10,14 +10,19 @@ class UsersController < ApplicationController
 
   def index
     @users = User.all
+    @user = current_user
     @book = Book.new
   end
 
   def edit
     @user = User.find(params[:id])
+    if @user != current_user
+      redirect_to user_path(current_user.id)
+    end
   end
 
   def update
+    @user = User.find(params[:id])
     if @user.update(user_params)
       redirect_to users_path(@user), notice: "You have updated user successfully."
     else
